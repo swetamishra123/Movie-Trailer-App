33c948b18579716c5b28eef9d84220780ab2e832
swetamishra123 <swetamishra603@gmail.com>
2024-02-26 00:51:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// App..js 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css'; 
+import { useState } from 'react'; 
+import ReactPlayer from 'react-player'; 
+import movieTrailer from 'movie-trailer'; 
+
+function App() { 
+
+	//Setting up the initial states using 
+	// react hook 'useState" 
+	const [video, setVideo] = useState("inception"); 
+	const [videoURL, setVideoURL] = 
+		useState("https://youtu.be/sa9l-dTv9Gk"); 
+
+	//A function to fetch the required URL 
+	// and storing it inside the 
+	// videoURL state variable 
+	function handleSearch() { 
+		movieTrailer(video).then((res) => { 
+			setVideoURL(res); 
+		}); 
+	} 
+
+	return ( 
+		<div className="App"> 
+			<div className="search-box"> 
+				<label> 
+					Search for any movies/shows:{" "} 
+				</label> 
+				<input type="text" onChange= 
+					{(e) => { setVideo(e.target.value) }} /> 
+
+				<button onClick={() => { handleSearch() }}> 
+					Search 
+				</button> 
+			</div> 
+
+	
+			<ReactPlayer url={videoURL} controls={true} /> 
+		</div> 
+	); 
+} 
 
 export default App;
